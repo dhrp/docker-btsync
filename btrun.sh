bd5f94c58d8a7ae44ab9ada1e071ab897c820f76
noopihc <chipoon@gmail.com>
2013-11-04 13:35:31 -0500

    Create btrun.sh

---
+++ btrun.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Checking default params
+LOGIN=${LOGIN:-admin}
+PASSWORD=${PASSWORD:-password}
+
+# Automating some btsync setup
+/usr/bin/btsync --dump-sample-config > /usr/bin/btsync.conf
+
+sed -i 's/"listening_port" : 0/"listening_port" : 55555/' /usr/bin/btsync.conf
+sed -i 's/"storage_path" : "\/home\/user\/.sync"/"storage_path" : "\/btsync\/.s$
+sed -i 's/"login" : "admin"/"login" : "'$LOGIN'"/' /usr/bin/btsync.conf
+sed -i 's/"password" : "password"/"password" : "'$PASSWORD'"/' /usr/bin/btsync.$
+
+/usr/bin/btsync --nodaemon --config /usr/bin/btsync.conf
