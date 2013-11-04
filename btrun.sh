f12c1f1eac62acd8a742de55f0cd4f48c74fd838
noopihc <chipoon@gmail.com>
2013-11-04 13:46:21 -0500

    Update btrun.sh

--- btrun.sh
+++ btrun.sh
@@ -8,8 +8,8 @@ PASSWORD=${PASSWORD:-password}
 /usr/bin/btsync --dump-sample-config > /usr/bin/btsync.conf
 
 sed -i 's/"listening_port" : 0/"listening_port" : 55555/' /usr/bin/btsync.conf
-sed -i 's/"storage_path" : "\/home\/user\/.sync"/"storage_path" : "\/btsync\/.s$
+sed -i 's/"storage_path" : "\/home\/user\/.sync"/"storage_path" : "\/btsync\/.sync"/' /usr/bin/btsync.conf
 sed -i 's/"login" : "admin"/"login" : "'$LOGIN'"/' /usr/bin/btsync.conf
-sed -i 's/"password" : "password"/"password" : "'$PASSWORD'"/' /usr/bin/btsync.$
+sed -i 's/"password" : "password"/"password" : "'$PASSWORD'"/' /usr/bin/btsync.conf
 
 /usr/bin/btsync --nodaemon --config /usr/bin/btsync.conf
